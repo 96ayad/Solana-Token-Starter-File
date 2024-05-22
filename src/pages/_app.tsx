ee5781ff06a3773d783dae1c7b7c5bcd780adcd0
ayadag <ayad.ali.g@gmail.com>
2024-05-22 21:08:59 +0300

    _aap.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,8 @@ import { FC } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import { AppBar } from '../components/AppBar';
-import { Footer } from '../components/Footer';
+// import { AppBar } from '../components/AppBar';
+// import { Footer } from '../components/Footer';
 import Notifications from '../components/Notification';
 import { ContextProvider } from '../contexts/ContextProvider';
 
@@ -19,9 +19,9 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
       </Head>
       <ContextProvider>
         <Notifications />
-        <AppBar />
+        {/* <AppBar /> */}
         <Component {...pageProps} />
-        <Footer />
+        {/* <Footer /> */}
       </ContextProvider>
 
       {/* //SCRIPTS */}
