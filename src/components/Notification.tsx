651dd5053b89535bc8194bb8cc651f1af0a65e07
96ayad <96ayad@gmail.com>
2024-05-23 14:52:17 +0300

    Notification

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+// import React, { useEffect, useState } from 'react'
+// import { 
+//   CheckCircleIcon,
+//   InformationCircleIcon,
+//   XCircleIcon, 
+// } from "@heroicons/react/outline";
+// import { XIcon } from "@heroicons/react/solid";
+// import useNotificationStore from "../stores/useNotificationStore";\
+// import { useConnection } from '@solana/wallet-adapter-react';
+// import { useNetworkConfiguration } from '../contexts/NetworkConfigurationProvider';
+// import { Message } from '@solana/web3.js';
 
-const Notification = () => {
-  return (
-    <div>Notification</div>
-  )
-}
+// //INTERNAL IMPORT
 
-export default Notification
+// const NotificationList = () => {
+//   const {notifications, set: setNotificationStore} = useNotificationStore(
+//     (s) => s
+//   );
+
+//   const reversedNotifications = [...notifications].reverse();
+//   return (
+//   <div
+//     className={`pointer-events-none fixed inset-0 z-20 flex items-end px-4 py-6
+//     sm:p-6`}
+//     >
+//     <div className={'flex w-full flex-col'}>
+//       {reversedNotifications.map((n, idx) => (
+//         <Notification 
+//         key={`${n.message}${idx}`}
+//         type={n.type}
+//         message={n.message}
+//         description={n.description}
+//         txid={n.txid}
+//         onHide={() => {
+//           setNotificationStore((state: any) => {
+//             const reversedIndex = reversedNotifications.length - 1 - idx;
+//             state.notifications = [
+//               ...notifications.slice(0, reversedIndex),
+//               ...notifications.slice(reversedIndex + 1),
+//             ]
+//           });
+//         }}
+//         />
+//       ))}
+//     </div>
+//   </div>
+// );
+// };
+
+// const Notification = ({ type, message, description, txid, onHide }) => {
+//   const { connection } = useConnection();
+//   const { networkConfiguration } = useNetworkConfiguration();
+
+//   useEffect(() => {
+//     const id = setTimeout(() => {
+//       onHide();
+//     },8000);
+
+//     return () => {
+//       clearInterval(id);
+//     };
+//   }, [onHide]);
+// };
+
+// export default NotificationList;
