3691d9827a79e953f8c913d9441b909c10262df7
ayadag <ayad.ali.g@gmail.com>
2024-05-24 10:36:19 +0300

    view

---
+++ src/views/index.tsx
@@ -0,0 +1,11 @@
+export { HomeView } from './home';
+export { ToolView } from './tools';
+export { FeatureView } from './feature';
+export { OfferView } from './offer';
+export { FaqView } from './faq';
+export { AirdropView } from './airdrop';
+export { DonsteView } from './donate';
+export { CreateView } from './create';
+export { InputView } from './input';
+export { TokenMetata } from './tokenMetadata';
+export { ContactView } from './conatct';
